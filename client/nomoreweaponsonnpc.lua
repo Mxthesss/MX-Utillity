1097901bfa3b4014dd6a084fa198bb9071e66819
Mxthesss <bednar.matej2@seznam.cz>
2023-08-12 22:11:26 +0200

    Create nomoreweaponsonnpc.lua

---
+++ client/nomoreweaponsonnpc.lua
@@ -0,0 +1,27 @@
+if not mx.NoMoreWeaponsOnNPC then
+  return
+end
+
+if mx.NoMoreWeaponsOnNPC then
+
+  function SetWeaponDrops()
+	local handle, ped = FindFirstPed()
+	local finished = false
+
+	repeat
+		if not IsEntityDead(ped) then
+			SetPedDropsWeaponsWhenDead(ped, false)
+		end
+		finished, ped = FindNextPed(handle)
+	until not finished
+
+	EndFindPed(handle)
+end
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1000)
+		SetWeaponDrops()
+	end
+end)
+  

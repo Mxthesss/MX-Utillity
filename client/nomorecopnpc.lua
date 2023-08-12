0ef8fbcd172e56770640880c8a11a91e67e06ca9
Mxthesss <bednar.matej2@seznam.cz>
2023-08-12 22:13:18 +0200

    Create nomorecopnpc.lua

---
+++ client/nomorecopnpc.lua
@@ -0,0 +1,13 @@
+if not mx.NoMoreCopNPC then 
+  return
+end
+
+if mx.NoMoreCopNPC then
+  
+  Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        local myCoords = GetEntityCoords(GetPlayerPed(-1))
+        ClearAreaOfCops(myCoords.x, myCoords.y, myCoords.z, 100.0, 0)
+    end
+end)

ff3b645f76098416fe2bb5e59f5dd3aa36726269
Mxthesss <bednar.matej2@seznam.cz>
2023-08-12 23:49:37 +0200

    Create ragdoll.lua

---
+++ client/ragdoll.lua
@@ -0,0 +1,69 @@
+if not mx.RagDoll then
+  return
+end
+
+if mx.Ragdoll then
+
+local ragdoll = false
+local shownHelp = false
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		ESX = exports["es_extended"]:getSharedObject()
+		Citizen.Wait(0)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if IsControlJustPressed(2, 82) and not IsPedInAnyVehicle(GetPlayerPed(-1), false) then
+            ragdoll = not ragdoll
+            if not ragdoll then
+                shownHelp = false
+            end
+        end
+        if Config.stunShouldRagdoll and IsPedBeingStunned(GetPlayerPed(-1)) then
+            ragdoll = true
+        end
+
+        -- Don't ragdoll if player is dead
+        if IsPlayerDead(PlayerId()) and ragdoll == true then
+            ragdoll = false
+            shownHelp = false
+        end
+        if ragdoll == true and not shownHelp then
+            ESX.ShowHelpNotification("Press ~INPUT_VEH_PREV_RADIO~ to stand up")
+            shownHelp = true
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if ragdoll then
+            SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, 0, 0, 0)
+        end
+    end
+end)
+
+RegisterNetEvent('MX-RagDoll:toggle')
+AddEventHandler('MX-RagDoll:toggle', function()
+    ragdoll = not ragdoll
+    if not ragdoll then
+        shownHelp = false
+    end
+end)
+
+RegisterNetEvent('MX-RagDoll:set')
+AddEventHandler('MX-RagDoll:set', function(value)
+    ragdoll = value
+    if not ragdoll then
+        shownHelp = false
+    end
+end)
+
+RegisterCommand(mx.RagDollCommand, function (src, args, raw)
+    TriggerEvent("MX-RagDoll:toggle")
+end, false)
